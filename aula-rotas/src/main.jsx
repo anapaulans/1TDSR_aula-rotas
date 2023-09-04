bd5d93799615f20a0aa35af6cc5a92e783f50787
anapaulans <rm552513@fiap.com.br>
2023-09-04 15:29:25 -0300

    importando o elemento do router e as paginas no main.jsx

---
+++ aula-rotas/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import {createBrowserRouter, RouterProvider, Navigate} from 'react-router-dom'
+import Home from './routes/Home/Home.jsx'
+import Produtos from './routes/Produtos/Produtos.jsx'
+import Error from './routes/Error/Error.jsx'
+import EditarProduto from './routes/EditarProdutos/EditarProdutos.jsx'
