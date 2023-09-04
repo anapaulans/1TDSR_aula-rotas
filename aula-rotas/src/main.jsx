02b73738ff75cde6b3d588b362e06ff349c48f05
anapaulans <rm552513@fiap.com.br>
2023-09-04 15:32:56 -0300

    Usando RouterProvider, ReactDOM, no meu router

--- aula-rotas/src/main.jsx
+++ aula-rotas/src/main.jsx
@@ -32,3 +32,9 @@ const router = createBrowserRouter([{
     }
   ]
 }])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
