9a682b7c3b0f3f17e13fb78849b081a75df3416a
anapaulans <rm552513@fiap.com.br>
2023-09-04 15:56:14 -0300

    Ajustes

--- aula-rotas/src/routes/EditarProdutos/EditarProdutos.jsx
+++ aula-rotas/src/routes/EditarProdutos/EditarProdutos.jsx
@@ -1,25 +1,8 @@
-import {useParams, useNavigate } from "react-router-dom"
-import { ListaProdutos } from "../../components/ListaProdutos"
+export default function EditarProduto(){
 
-export default function EditarProduto() {
-
-  const lista = ListaProdutos
-  const navegacao = useNavigate()
-  const {id} = useParams()
-
-  const proc = lista.filter(prod => prod.id == id)
-  const produto = proc[0]
-  
-  const salvar = () => {
-    alert(`Produto: ${produto.nome} editado com sucesso!`)
-    return navegacao('/produtos')
-  }
-  
-  return (
+  return(
     <main>
-        <h1>Editando o produto</h1>
-        <p>Editando os dados do produto: {produto.nome}</p>
-        <button onClick={salvar}>Salvar</button>
+        <h1>Editando o produto </h1>
     </main>
   )
 }
