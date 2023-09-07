a739c2fc2a0ef82009b91ac379ea5a5b15bb249a
anapaulans <rm552513@fiap.com.br>
2023-09-06 21:43:54 -0300

    Usando useParams, useNavigate

--- aula-rotas/src/routes/EditarProdutos/EditarProdutos.jsx
+++ aula-rotas/src/routes/EditarProdutos/EditarProdutos.jsx
@@ -1,8 +1,25 @@
-export default function EditarProduto(){
+import {useParams, useNavigate } from "react-router-dom"
+import { ListaProdutos } from "../../components/ListaProdutos"
 
-  return(
+export default function EditarProduto() {
+
+  const lista = ListaProdutos
+  const navegacao = useNavigate()
+  const {id} = useParams()
+
+  const proc = lista.filter(prod => prod.id == id)
+  const produto = proc[0]
+  
+  const salvar = () => {
+    alert(`Produto: ${produto.nome} editado com sucesso!`)
+    return navegacao('/produtos')
+  }
+  
+  return (
     <main>
-        <h1>Editando o produto </h1>
+        <h1>Editando o produto</h1>
+        <p>Editando os dados do produto: {produto.nome}</p>
+        <button onClick={salvar}>Salvar</button>
     </main>
   )
 }
